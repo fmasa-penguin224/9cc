38ec8626f3a61f6701cbd13e4729b2386a8ea118
fmasa-penguin224 <masamoon2018@gmail.com>
2021-08-01 08:48:43 +0000

    addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -2,16 +2,37 @@
 #include<stdlib.h>
 
 int main(int argc, char **argv){
-   if (argc != 2){
-      fprintf(stderr, "incorrect\n");
-      return 1;
-   }
-   
-   printf(".intel_syntax noprefix\n");
-   printf(".globl main\n");
-   printf("main:\n");
-   printf("\tmov rax, %d\n", atoi(argv[1]));
-   printf("\tret\n");
-   
-   return 0;
+	if (argc != 2){
+		fprintf(stderr, "incorrect\n");
+		return 1;
+	}
+
+	char *p = argv[1];
+
+	printf(".intel_syntax noprefix\n");
+	printf(".globl main\n");
+	printf("main:\n");
+	printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+ 
+		if (*p == '-'){
+			p++;
+			printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		  
+		}
+
+		fprintf(stderr, "incorrect: '%c'\n", *p);
+		return 1;
+	}
+
+	printf("\tret\n");
+
+	return 0;
 }
